60b1ff05df1621de7af282f02dcca7045842f949
Mathew Shen <datahonor@gmail.com>
2024-04-18 22:04:01 +0800

    add: ngram counter (#5)

---
+++ src/ngram.rs
@@ -0,0 +1,36 @@
+use counter::Counter;
+
+
+pub fn get_ngram_counter(line: &str, max_order: usize) -> Counter<&str> {
+    let mut counts: Counter<&str> = Counter::new();
+    for order in 1..=max_order {
+        for start_index in 0..=(line.len() - order) {
+            let ngram = &line[start_index..(start_index + order)];
+            counts[&ngram] += 1;
+        }
+    }
+    counts
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::ngram::{get_ngram_counter};
+
+    #[test]
+    fn test_get_ngram() {
+        let counter = get_ngram_counter("aabc", 4);
+        assert_eq!(counter[&"a"], 2);
+        assert_eq!(counter[&"b"], 1);
+        assert_eq!(counter[&"c"], 1);
+        assert_eq!(counter[&"d"], 0);
+
+        assert_eq!(counter[&"aa"], 1);
+        assert_eq!(counter[&"ab"], 1);
+        assert_eq!(counter[&"bc"], 1);
+        assert_eq!(counter[&"ac"], 0);
+
+        assert_eq!(counter[&"aab"], 1);
+        assert_eq!(counter[&"aabc"], 1);
+    }
+}
