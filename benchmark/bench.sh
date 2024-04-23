d03b5222a1558eb6e2a9235d5efd520dd2abd8b9
Xiangzhuang Shen <datahonor@gmail.com>
2024-04-23 22:52:53 +0800

    add: sacrebleu benchmark

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -1,1 +1,4 @@
-hyperfine --warmup 5 --runs 100 "python simple/rs_bleuscore.py" "python simple/hf_evaluate.py"
+hyperfine --warmup 5 --runs 100   \
+  "python simple/rs_bleuscore.py" \
+  "python simple/sacre_bleu.py"   \
+  "python simple/hf_evaluate.py"
