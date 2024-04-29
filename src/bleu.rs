999dad39fdc262d55687f65962286556d7b885a8
Xiangzhuang Shen <datahonor@gmail.com>
2024-04-29 17:16:37 +0800

    add: rayon

--- src/bleu.rs
+++ src/bleu.rs
@@ -1,10 +1,8 @@
 use crate::ngram::get_token_ngram_counter;
 use crate::tokenizer::{Tokenizer, Tokenizer13a};
+use rayon::prelude::*;
 use std::cmp::min;
 use std::collections::HashMap;
-use std::time::{Duration, Instant};
-use rayon::prelude::*;
-use std::sync::{Arc, Mutex};
 
 /// The BLEU score data struct
 #[derive(Debug, Default)]
@@ -17,6 +15,20 @@ pub struct BleuScore {
     pub reference_length: usize,
 }
 
+fn add_vectors(vec1: Vec<usize>, vec2: Vec<usize>) -> Vec<usize> {
+    // Ensure both vectors have the same length
+    assert_eq!(vec1.len(), vec2.len(), "Vectors must have the same length");
+
+    // Add elements of vec1 and vec2 element by element
+    let result: Vec<_> = vec1
+        .into_iter()
+        .zip(vec2.into_iter())
+        .map(|(a, b)| a + b)
+        .collect();
+
+    result
+}
+
 /// compute the BLEU score with `Tokenizer13a` as default tokenizer.
 /// The implementation is based on [huggingface/nmt](https://github.com/huggingface/evaluate/blob/main/metrics/bleu/bleu.py)
 pub fn compute_score(
@@ -25,74 +37,86 @@ pub fn compute_score(
     max_order: usize,
     smooth: bool,
 ) -> BleuScore {
-    let bleu_start = Instant::now();
-    // init
-    let matches_by_order = Arc::new(Mutex::new(vec![0; max_order]));
-    let possible_matches_by_order = Arc::new(Mutex::new(vec![0; max_order]));
-    let reference_length = Arc::new(Mutex::new(0usize));
-    let translation_length = Arc::new(Mutex::new(0usize));
     let tokenizer = Tokenizer13a::new();
 
-    let mut tokenize_duration_sum = Duration::new(0, 0);
-
     // tokenize
-    let tokenize_start = Instant::now();
-    let token_paris: Vec<_> = references.into_par_iter().zip(predictions.into_par_iter()).map(
-        |(references, translation)| {
+    let result = references
+        .into_par_iter()
+        .zip(predictions.into_par_iter())
+        .map(|(references, translation)| {
             let translation_tokens = tokenizer.tokenize(translation);
             let references_tokens: Vec<Vec<String>> =
                 references.iter().map(|x| tokenizer.tokenize(x)).collect();
-            (references_tokens, translation_tokens)
-        }
-    ).collect();
-    tokenize_duration_sum += tokenize_start.elapsed();
 
-    // length count & possible match
-    token_paris.par_iter().for_each(|(references_tokens, translation_tokens)| {
-        *reference_length.lock().unwrap() += references_tokens.iter().map(|x| x.len()).min().unwrap();
-        *translation_length.lock().unwrap() += translation_tokens.len();
+            let reference_length = references_tokens.iter().map(|x| x.len()).min().unwrap();
+            let translation_length = translation_tokens.len();
 
-        for order in 1..=max_order {
-            let possible_matches = translation_tokens.len().saturating_sub(order - 1);
-            if possible_matches > 0 {
-                possible_matches_by_order.lock().unwrap()[order - 1] += possible_matches
+            let mut possible_matches_by_order = vec![0; max_order];
+            for order in 1..=max_order {
+                let possible_matches = translation_tokens.len().saturating_sub(order - 1);
+                if possible_matches > 0 {
+                    possible_matches_by_order[order - 1] += possible_matches
+                }
             }
-        }
 
-        let translation_ngram_counts = get_token_ngram_counter(&translation_tokens, max_order);
-        let mut merged_ref_ngram_counts = HashMap::new();
-        for reference_tokens in references_tokens.iter() {
-            let reference_ngram_counts = get_token_ngram_counter(reference_tokens, max_order);
-            for (key, value) in reference_ngram_counts {
-                merged_ref_ngram_counts
-                    .entry(key)
-                    .and_modify(|v| *v += value)
-                    .or_insert(value);
+            let translation_ngram_counts = get_token_ngram_counter(&translation_tokens, max_order);
+            let mut merged_ref_ngram_counts = HashMap::new();
+            for reference_tokens in references_tokens.iter() {
+                let reference_ngram_counts = get_token_ngram_counter(reference_tokens, max_order);
+                for (key, value) in reference_ngram_counts {
+                    merged_ref_ngram_counts
+                        .entry(key)
+                        .and_modify(|v| *v += value)
+                        .or_insert(value);
+                }
             }
-        }
 
-        // overlap count
-        for (k, v) in translation_ngram_counts {
-            if merged_ref_ngram_counts.contains_key(k) {
-                matches_by_order.lock().unwrap()[k.len() - 1] += min(merged_ref_ngram_counts[k], v);
-            } else {
-                continue;
+            // overlap count
+            let mut matches_by_order = vec![0; max_order];
+            for (k, v) in translation_ngram_counts {
+                if merged_ref_ngram_counts.contains_key(k) {
+                    matches_by_order[k.len() - 1] += min(merged_ref_ngram_counts[k], v);
+                } else {
+                    continue;
+                }
             }
-        }
-    });
+            (
+                translation_length,
+                reference_length,
+                possible_matches_by_order,
+                matches_by_order,
+            )
+        })
+        .reduce_with(
+            // (0usize, 0usize, vec![0usize; max_order], vec![0usize; max_order]),
+            |s1, s2| {
+                (
+                    s1.0 + s2.0,
+                    s1.1 + s2.1,
+                    add_vectors(s1.2, s2.2),
+                    add_vectors(s1.3, s2.3),
+                )
+            },
+        );
+
+    let (translation_length, reference_length, possible_matches_by_order, matches_by_order) =
+        match result {
+            None => panic!(""),
+            Some(stats) => stats,
+        };
 
     // precisions calculation
     let mut precisions: Vec<f64> = vec![0.0; max_order];
     for i in 0..max_order {
         match smooth {
             true => {
-                precisions[i] = (matches_by_order.lock().unwrap()[i] as f64 + 1.0)
-                    / (possible_matches_by_order.lock().unwrap()[i] as f64 + 1.0);
+                precisions[i] = (matches_by_order[i] as f64 + 1.0)
+                    / (possible_matches_by_order[i] as f64 + 1.0);
             }
             false => {
-                if possible_matches_by_order.lock().unwrap()[i] > 0 {
+                if possible_matches_by_order[i] > 0 {
                     precisions[i] =
-                        (matches_by_order.lock().unwrap()[i] as f64) / (possible_matches_by_order.lock().unwrap()[i] as f64)
+                        (matches_by_order[i] as f64) / (possible_matches_by_order[i] as f64)
                 } else {
                     precisions[i] = 0.0;
                 }
@@ -101,31 +125,27 @@ pub fn compute_score(
     }
 
     let mut geo_mean = 0.0;
-    
+
     if precisions.iter().fold(f64::INFINITY, |a, &b| a.min(b)) > 0.0 {
         let p_log_sum: f64 =
             (1.0 / max_order as f64) * precisions.iter().map(|&x| x.ln()).sum::<f64>();
         geo_mean = p_log_sum.exp();
     }
 
-    let length_ratio: f64 = *translation_length.lock().unwrap() as f64 / *reference_length.lock().unwrap() as f64;
+    let length_ratio: f64 = translation_length as f64 / reference_length as f64;
     let mut brevity_penalty = 1.0;
     if length_ratio <= 1.0 {
         brevity_penalty = (1.0 - 1.0 / length_ratio).exp();
     }
     let bleu = geo_mean * brevity_penalty;
-    
-    println!("bleu duration: {:?}", bleu_start.elapsed());
-    println!("tokenize duration: {:?}", tokenize_duration_sum);
-
 
     BleuScore {
         bleu,
         precisions,
         brevity_penalty,
         length_ratio,
-        translation_length: *translation_length.clone().lock().unwrap(),
-        reference_length: *reference_length.clone().lock().unwrap(),
+        translation_length,
+        reference_length,
     }
 }
 
