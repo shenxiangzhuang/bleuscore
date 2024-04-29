738d3493152147a505898bbe8125c28615551b67
Xiangzhuang Shen <datahonor@gmail.com>
2024-04-29 19:50:48 +0800

    fix

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -1,5 +1,5 @@
 hyperfine --warmup 5 --runs 100   \
   "python simple/rs_bleuscore.py" \
   "python simple/local_hf_bleu.py" \
-#  "python simple/sacre_bleu.py"   \
-#  "python simple/hf_evaluate.py"
+  "python simple/sacre_bleu.py"   \
+  "python simple/hf_evaluate.py"
