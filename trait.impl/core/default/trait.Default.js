1166b72dbbe3c6def771e8c7ff4c5df969028269
shenxiangzhuang <shenxiangzhuang@users.noreply.github.com>
2025-03-25 16:50:03 +0000

    deploy: fe3983ff4646f091192a2c63e401943cfee90737

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bleuscore",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"bleuscore/struct.BleuScore.html\" title=\"struct bleuscore::BleuScore\">BleuScore</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"bleuscore/struct.Tokenizer13a.html\" title=\"struct bleuscore::Tokenizer13a\">Tokenizer13a</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"bleuscore/struct.TokenizerRegex.html\" title=\"struct bleuscore::TokenizerRegex\">TokenizerRegex</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[852]}
