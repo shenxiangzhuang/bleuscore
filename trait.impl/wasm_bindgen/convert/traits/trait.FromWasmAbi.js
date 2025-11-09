adb3aeb63e8b68c25a5c24447b8ceedfbf3bc368
shenxiangzhuang <shenxiangzhuang@users.noreply.github.com>
2025-11-09 06:41:57 +0000

    deploy: 7f7491e79b40125c6f913433e05d2e0245468b6f

---
+++ trait.impl/wasm_bindgen/convert/traits/trait.FromWasmAbi.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bleuscore_js",[["impl <a class=\"trait\" href=\"https://docs.rs/wasm-bindgen/0.2/wasm_bindgen/convert/traits/trait.FromWasmAbi.html\" title=\"trait wasm_bindgen::convert::traits::FromWasmAbi\">FromWasmAbi</a> for <a class=\"struct\" href=\"bleuscore_js/bleu/struct.BleuScore.html\" title=\"struct bleuscore_js::bleu::BleuScore\">BleuScore</a><div class=\"where\">where\n    Self: <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.DeserializeOwned.html\" title=\"trait serde_core::de::DeserializeOwned\">DeserializeOwned</a>,</div>"],["impl <a class=\"trait\" href=\"https://docs.rs/wasm-bindgen/0.2/wasm_bindgen/convert/traits/trait.FromWasmAbi.html\" title=\"trait wasm_bindgen::convert::traits::FromWasmAbi\">FromWasmAbi</a> for <a class=\"struct\" href=\"bleuscore_js/bleu/struct.VecString.html\" title=\"struct bleuscore_js::bleu::VecString\">VecString</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/wasm-bindgen/0.2/wasm_bindgen/convert/traits/trait.FromWasmAbi.html\" title=\"trait wasm_bindgen::convert::traits::FromWasmAbi\">FromWasmAbi</a> for <a class=\"struct\" href=\"bleuscore_js/bleu/struct.VecVecString.html\" title=\"struct bleuscore_js::bleu::VecVecString\">VecVecString</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1236]}
---
+++ trait.impl/wasm_bindgen/convert/traits/trait.VectorIntoWasmAbi.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bleuscore_js",[["impl <a class=\"trait\" href=\"https://docs.rs/wasm-bindgen/0.2/wasm_bindgen/convert/traits/trait.VectorIntoWasmAbi.html\" title=\"trait wasm_bindgen::convert::traits::VectorIntoWasmAbi\">VectorIntoWasmAbi</a> for <a class=\"struct\" href=\"bleuscore_js/bleu/struct.BleuScore.html\" title=\"struct bleuscore_js::bleu::BleuScore\">BleuScore</a><div class=\"where\">where\n    <a class=\"struct\" href=\"bleuscore_js/bleu/struct.BleuScore.html\" title=\"struct bleuscore_js::bleu::BleuScore\">BleuScore</a>: <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[691]}
